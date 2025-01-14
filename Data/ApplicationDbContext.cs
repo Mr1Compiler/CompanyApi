e83db31787d401f2691eb8cce917a4451dd09f90
Ayman H <aymen9255@gmail.com>
2025-01-14 12:32:38 +0300

    Complete database basic setup

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using CompanyApi.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyApi.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions options) : base(options)
+		{
+		}
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<Employee>()
+				.HasOne(e => e.Department)
+				.WithMany(d => d.Employees)
+				.HasForeignKey(e => e.DepartmentId)
+				.OnDelete(DeleteBehavior.Restrict);
+		}
+
+		DbSet<Employee> Employees { get; set; }
+		DbSet<Department> Departments { get; set; }
+	}
+}
---
+++ Models/Entities/Employee.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace CompanyApi.Models.Entities
+{
+	public class Employee
+	{
+		public int Id { get; set; }
+		[Required]
+		public required string Username { get; set; }
+		public required string Password { get; set; }	
+		public string? Name { get; set; }
+		public string Role { get; set; } = "Employee";
+		public DateTime? HireDate { get; set; }
+		public int? DepartmentId { get; set; }
+		public Department? Department { get; set; }
+	}
+}
